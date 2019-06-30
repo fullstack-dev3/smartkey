93dc311460279c93341a87860fd472e3ec77a7e3
Shark <jack.qiao123@gmail.com>
2019-06-30 19:01:22 +0300

    Form Design

--- index.php
+++ index.php
@@ -5,24 +5,26 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <body>
 
-<h3 class="text-center">Enter Here</h3>
+<div class="row">	
+	<h3 class="text-center">Enter Here</h3>
 
-<form action="" method="POST">
-	<div class="form-group">
-		<label for="username">Name</label>
-		<input type="text" class="form-control" name="username">
-	</div>
-	<div class="form-group">
-		<label for="email">Email</label>
-		<input type="text" class="form-control" name="email">
-	</div>
+	<form action="" method="POST">
+		<div class="form-group">
+			<label for="username">Name</label>
+			<input type="text" class="form-control" name="username">
+		</div>
+		<div class="form-group">
+			<label for="email">Email</label>
+			<input type="text" class="form-control" name="email">
+		</div>
 
-	<input type="submit" class="btn btn-primary" value="Submit">
-</form>
+		<input type="submit" class="btn btn-primary" value="Submit">
+	</form>
+</div>
 
 </body>
 </html>
