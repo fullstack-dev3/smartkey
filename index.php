b4f644f417876c71f8ce5a372f8b853087b4c67c
Shark <jack.qiao123@gmail.com>
2019-06-30 18:53:58 +0300

    Form Design

--- index.php
+++ index.php
@@ -4,10 +4,13 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
 
-<h3>Enter Here</h3>
+<h3 class="text-center">Enter Here</h3>
+
 <form action="" method="POST">
 	<label for="username">Name</label>
 	<input type="text" name="username">
