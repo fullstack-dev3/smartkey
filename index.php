e06d89235aadc427319f4fff6ce618cc2ecc4a68
Shark <jack.qiao123@gmail.com>
2019-06-30 18:49:30 +0300

    Form Design

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
-	echo 123456789;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+
+<h3>Enter Here</h3>
+<form action="" method="POST">
+	<label for="username">Name</label>
+	<input type="text" name="username">
+
+	<label for="email">Name</label>
+	<input type="text" name="email">
+
+	<input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
