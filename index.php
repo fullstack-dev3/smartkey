92cda618dfb179bb57821866dd8e7916216f44f5
Shark <jack.qiao123@gmail.com>
2019-07-02 21:48:32 +0300

    Airtable API

--- index.php
+++ index.php
@@ -15,10 +15,10 @@ if (isset($_POST['username']) && isset($_POST['email'])) {
 		'Email' => $_POST['email'],
 		'Giveaway Number' => rand(10000, 99000)
 	);
-
+var_dump($data);
 	$new = $airtable->saveContent('Table 1', $data);
 
-	echo $new->id;
+	echo $new->id;exit;
 }
 
 ?>
